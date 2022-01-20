9a753c21e15e9dcfe31bebed338b27ce8827cd40
Lesly Rojas <leslyrojast@gmail.com>
2022-01-20 13:52:05 -0300

    se agregan link y botones a home

---
+++ src/Components/Cocina.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Cocina = () => {
+  return ( 
+    <h1>Aquí va la cocina</h1>
+  );
+}
+
+export  { Cocina };
--- src/Components/Desayunos.jsx
+++ src/Components/Desayunos.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Desayunos = () => {
   return ( 
     <h1>Aqui va lista desayunos</h1>
-   );
+  );
 }
- 
+
 export  { Desayunos }
