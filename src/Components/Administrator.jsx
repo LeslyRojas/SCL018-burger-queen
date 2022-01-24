a46c131b2d6e81cd816a0628867fd322218ba91b
Lesly Rojas <leslyrojast@gmail.com>
2022-01-24 16:24:32 -0300

    eslint administrator

--- src/Components/Administrator.jsx
+++ src/Components/Administrator.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function Administrator() {
-  return <h1>Aquí va la vista de administrador</h1>;
+  return (
+    <>
+      <h1>Aquí va la vista de administrador</h1>
+      <h1>Aquí va la vista de administrador</h1>
+    </>
+  );
 }
 
 export default Administrator;
